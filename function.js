385347d73d559dd436b22482bbf8a2f0b1f5092c
J.I. Jahid <j.i.jahid90@gmail.com>
2025-10-24 22:00:15 +0600

    self invoked function added

--- function.js
+++ function.js
@@ -1,5 +1,6 @@
+//Function syntex
 function myFunction() {
-  console.log("hello Javascript");
+//   console.log("hello Javascript");
 }
 
 myFunction();
@@ -7,7 +8,21 @@ myFunction();
 
 // ()----> Parameter
 function name(name, age ){
-    console.log("My name is " + name +"." + " I am " + age + " years old");
+    // console.log("My name is " + name +"." + " I am " + age + " years old");
 }
 name("Jahid", 21)
 // ()----> Argument
+
+
+// ehkane (dateOfBirth = 2000) kaj korbe tohkon ei johkon user date of birth er data dibe na. otherwise jodi na dea tahole consol.log hobe 2000. 
+function firstName(name, age, dateOfBirth = 2000 ){
+    // console.log("My name is " + name +"." + " I am " + age + " years old. " + "My Date of Birth " + dateOfBirth);
+}
+// firstName("Jahid", 21 , 2004)
+
+
+
+//Automatic (Self-invoked) invocation Function( ja nije nijei kaj kore)
+(function (message){
+    console.log("I am a Self-invocation", message);
+})("hello");
